879df3a6bf60df68c5e305570c6d47a746a1105c
cabosh <callum.bosher@gmail.com>
2020-06-03 11:23:32 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    num1 = int(input("Enter your first number: "))
+    num2 = int(input("Enter your second number: "))
+
+    if num1 > num2:
+        print(f"The greater number is: {num1}")
+    elif num2 > num1:
+        print(f"The greater number is: {num2}")
+    elif num1 == num2:
+        print("The numbers are equal!")
 
 if __name__ == '__main__':
     main()
